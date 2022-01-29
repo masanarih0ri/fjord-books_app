4d021a7a13ca6569a7258d521b385d4a6dd880cd
masanarih0ri <masanarihoridevelop@gmail.com>
2022-01-29 14:26:33 +0900

    add register and login feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    books_path
+  end
+
+  def after_sign_out_path_for(resource)
+    user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:zipcode, :address, :description])
+  end
 end
