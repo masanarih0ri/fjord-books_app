1f1f84a6dca4f3c86bf7f7b12591bc8e3ab2412e
masanarih0ri <masanarihoridevelop@gmail.com>
2022-01-30 19:09:16 +0900

    add feature to update user data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:zipcode, :address, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:zipcode, :address, :description])
   end
 end
