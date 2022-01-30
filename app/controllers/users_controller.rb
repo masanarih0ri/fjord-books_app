8a99058876e4eefacf666c2d2f94a9b44db5d733
masanarih0ri <masanarihoridevelop@gmail.com>
2022-01-30 13:32:35 +0900

    add show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
   def index
     @users = User.order(:id).page(params[:page]).per(5)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
