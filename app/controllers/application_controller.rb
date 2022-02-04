20dade82cc883e9b4d42cc48d5abe165a4a36530
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-04 10:11:20 +0900

    fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     user_session_path
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:zipcode, :address, :description])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:zipcode, :address, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[zipcode address description])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[zipcode address description])
   end
 end
