8d066c4a03c2cf2edf59d87d3a96d7f22c59edc7
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-05 15:53:26 +0900

    restrict upload file type

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
+
+  validate :check_content_type, if: :attached?
+
+  def check_content_type
+    extension = ['image/png', 'image/jpeg', 'image/gif']
+    errors.add(:image, 'の拡張子が間違っています。.png .jpg .jpeg .gifのいずれかにしてください。') unless image.content_type.in?(extension)
+  end
+
+  private
+
+  def attached?
+    image.attached?
+  end
 end
