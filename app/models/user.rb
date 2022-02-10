6c39d100ef4bf243dc5afc525b013dbd367d76aa
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-10 23:22:36 +0900

    add follow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,19 @@ class User < ApplicationRecord
   has_many :follower_relationships, class_name: 'FollowRelationship', foreign_key: 'follow_id'
   has_many :followings, through: :following_relationships, source: :follow
   has_many :followers, through: :following_relationships, source: :user
+
+  def follow(user)
+    unless self == user
+      self.following_relationships.find_or_create_by(follow_id: user.id)
+    end
+  end
+
+  def unfollow(user)
+    follow_relationship = self.following_relationships.find_by(follow_id: user.id)
+    follow_relationship.desroy if follow_relationship.present?
+  end
+
+  def following?(user)
+    self.followings.include?(user)
+  end
 end
