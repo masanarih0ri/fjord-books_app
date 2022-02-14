4381c5d1c591e60995803e203e57ea23bcc89f0d
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-14 23:32:06 +0900

    add CRUD for reports

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class ReportsController < ApplicationController
+  def index
+    @reports = Report.order(:id).page(params[:page])
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      flash[:notice] = t('.notice')
+      redirect_to reports_path
+    else
+      flash[:alert] = t('.alert')
+      render :new
+    end
+  end
+
+  def edit
+    @report = Report.find(params[:id])
+  end
+
+  def update
+    @report = Report.find(params[:id])
+    if @report.update(report_params)
+      flash[:notice] = t('.notice')
+    else
+      flash[:alert] = t('.alert')
+    end
+    redirect_to edit_report_path(@report)
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def destroy
+    @report = Report.find(params[:id])
+    if @report.destroy
+      flash[:notice] = t('.notice')
+      redirect_to reports_path
+    else
+      flash[:alert] = t('.alert')
+      redirect_to report_path(@report)
+    end
+
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
+end
