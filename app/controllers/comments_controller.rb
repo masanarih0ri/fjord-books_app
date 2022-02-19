f4f5398c13a0ba262ac5702eb342903842ba8af8
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-19 18:00:21 +0900

    add simple create comment feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = 'コメントを作成しました'
+    else
+      flash[:alert] = 'コメントを作成できませんでした'
+    end
+    redirect_to @commentable
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
