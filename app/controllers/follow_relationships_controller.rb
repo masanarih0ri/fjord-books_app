53b92f64d9f4a82713419678ebc1e9717d3fcec3
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-20 10:01:17 +0900

    modify destroy logic

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -13,14 +13,13 @@ class FollowRelationshipsController < ApplicationController
   end
 
   def destroy
-    @follow_user = User.find(params[:follow_id])
     @follow_relationship = FollowRelationship.find(params[:id])
     if @follow_relationship.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
     else
       flash[:danger] = 'ユーザーのフォローを解除できませんでした'
     end
-    redirect_to user_path(@follow_user)
+    redirect_to user_path(@follow_relationship.follow.id)
   end
 
   private
