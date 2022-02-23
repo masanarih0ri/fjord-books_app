381720aa9bd1d735702c11b4d33f8deb671f1cf5
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-23 20:24:32 +0900

    add reports system test

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,9 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#created_on' do
+    user = User.new(id: 1, email: 'foo@example.com', name: 'foo')
+    report = Report.new(id: 1, user_id: user.id, title: '日報タイトル', content: 'こんにちは', created_at: Time.zone.now, updated_at: Time.zone.now)
+    assert_equal Time.zone.today, report.created_on
+  end
 end
