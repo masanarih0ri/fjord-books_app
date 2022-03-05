e6f2fb59c27566fdee36580066d2fd09bcf4fc67
masanarih0ri <masanarihoridevelop@gmail.com>
2022-03-05 14:35:28 +0900

    modify tests for using fixtures

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,15 +4,15 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#created_on' do
-    user = User.new(id: 1, email: 'alice@example.com', name: 'alice')
-    report = Report.new(id: 1, user_id: user.id, title: '日報タイトル', content: 'こんにちは', created_at: Time.zone.now, updated_at: Time.zone.now)
+    user = users(:alice)
+    report = reports(:first_report)
     assert_equal Time.zone.today, report.created_on
   end
 
   test '#editable?' do
-    user1 = User.new(id: 1, email: 'alice@example.com', name: 'alice')
-    user2 = User.new(id: 2, email: 'bob@example.com', name: 'bob')
-    report = Report.new(id: 1, user_id: user1.id, title: '日報タイトル', content: 'こんにちは', created_at: Time.zone.now, updated_at: Time.zone.now)
+    user1 = users(:alice)
+    user2 = users(:bob)
+    report = reports(:first_report)
     report.user = user1
     assert report.editable?(user1)
     assert_not report.editable?(user2)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test '#name_or_email' do
-    user = User.new(email: 'foo@example.com', name: '')
+    user = users(:name_blank_user)
     assert_equal 'foo@example.com', user.name_or_email
 
     user.name = 'foobar'
@@ -12,39 +12,39 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test '#follow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+    alice = users(:alice)
+    bob = users(:bob)
+    
+    assert_not alice.following?(bob)
+    alice.follow(bob)
+    assert alice.following?(bob)
   end
 
   test '#unfollow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
+    alice = users(:alice)
+    bob = users(:bob)
 
-    me.follow(she)
-    assert me.following?(she)
-    me.unfollow(she)
-    assert_not me.following?(she)
+    alice.follow(bob)
+    assert alice.following?(bob)
+    alice.unfollow(bob)
+    assert_not alice.following?(bob)
   end
 
   test '#following?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
+    alice = users(:alice)
+    bob = users(:bob)
 
-    me.follow(she)
+    alice.follow(bob)
 
-    assert me.following?(she)
+    assert alice.following?(bob)
   end
 
   test '#followed_by?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
+    alice = users(:alice)
+    bob = users(:bob)
 
-    me.follow(she)
+    alice.follow(bob)
 
-    assert she.followed_by?(me)
+    assert bob.followed_by?(alice)
   end
 end
